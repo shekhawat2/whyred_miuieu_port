ee3c6fd43839896fc1a22478a04ea54035bf2e25
Anand Shekhawat <anandsingh215@yahoo.com>
2020-10-17 16:54:59 +0000

    update service_jar patch

--- whyred/patch/services_jar.sh
+++ whyred/patch/services_jar.sh
@@ -7,13 +7,7 @@ mkdir -p "$TMPDIR/original_dex"
 7z e "$LOCALDIR/system/system/framework/services.jar" classes.dex -o"$TMPDIR/original_dex" > /dev/null
 java -jar "$BAKSMALIJAR" disassemble "$TMPDIR/original_dex/classes.dex" -o "$TMPDIR/dexout"
 
-files=($TMPDIR/dexout/com/android/server/lights/LightsService.smali \
-       $TMPDIR/dexout/com/android/server/audio/AudioService.smali \
-       $TMPDIR/dexout/com/android/server/DisplayFeatureControl.smali)
-for file in ${files[@]}; do
-    sed -i "s/whyred//g" $file
-    sed -i "s/lavender/whyred/g" $file
-done
+rsync -ra $LOCALDIR/whyred/patch/services_jar/ $TMPDIR/dexout
 
 java -jar "$SMALIJAR" assemble "$TMPDIR/dexout" -o "$TMPDIR/classes.dex"
 zip -gjq "$LOCALDIR/system/system/framework/services.jar" "$TMPDIR/classes.dex"
