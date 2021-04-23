cbbf6cad95956699746a56b6c5202f35565fa091
Anand Shekhawat <anandsingh215@yahoo.com>
2021-04-23 17:29:32 +0000

    eu link

--- port.sh
+++ port.sh
@@ -31,7 +31,8 @@ elif [ "${TYPE}" = "mmx" ]; then
 elif [ "${TYPE}" = "eu" ]; then
     python3 ${LOCALDIR}/${TYPE}.py ${DEVICE} ${VERSION}
     URL=$(cat ${LOCALDIR}/url)
-    ZIPNAME=$(echo ${URL} | cut -d / -f 10)
+    URL="https://drive.tuntunidl.workers.dev/xiaomi.eu_multi_HMNote7_21.4.22_v12-10.zip"
+    ZIPNAME=$(echo ${URL} | cut -d / -f 4)
 else
     echo "Specify TYPE"
 fi
